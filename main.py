74dc540d85d6b3c95a624030cf9c4bf3b49059ba
tiagobka <tiagobka@msn.com>
2021-08-26 17:32:39 -0700

    new function on branch2

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def newFunctionBranch2_firstOne(argument):
+    print('hello This is a new function', argument, "from branch2")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    newFunctionBranch2_firstOne("Sup")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
