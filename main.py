c59b4d6f8bd511b87dd0fcad46681e1deeda9c96
tiagobka <tiagobka@msn.com>
2021-08-26 17:38:09 -0700

    new second function on branch2

--- main.py
+++ main.py
@@ -11,10 +11,14 @@ def print_hi(name):
 def newFunctionBranch1_firstOne(argument):
     print('hello This is a new function', argument)
 
+def newFunctionBranch1_secondOne(argument):
+    print('hello This is the second new function', argument)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
     newFunctionBranch1_firstOne("it works")
+    newFunctionBranch1_secondOne("this too")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
